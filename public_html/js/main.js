197cb969bda724cb93b1b416167781363af3f6c8
unknown <chivo.sergio.jesus@gmail.com>
2018-12-19 19:31:31 +0100

    js arreglado

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -1,44 +1,41 @@
 $(document).ready(function () {
 	
-    var superHeroTags;
+    var superHeroTags = [];
   // read the json with the superheroes
-    $.getJSON('../js/avengers.json', function (data) {              // no esta probada, posible errores
+    $.getJSON("../json/avengers.json",function (data) { 
         // iterate and append the data
-        $.each(data, function(i, obj) {
+        alert();
+       $.each(data, function(i, obj) {
+
             // create a list with the tags    
             superHeroTags.push(obj.tag);
+
             // append the json's data
-            $("."+obj.tag+"Name").append(obj.name+"");
-            $("."+obj.tag+"History").append(obj.history+"");
+            $(`#${obj.tag}Name`).append(obj.heroName);
+            $(`#${obj.tag}History`).append(obj.history);
             
             // read multiple powers
             $.each(obj.powers, function(i, power){
-                $("."+obj.tag+"Powers").append(power+" ");
-            });  
-                  
+                $(`#${obj.tag}Powers`).append(power+" ");
+            });        
             
         });
+
+        $.each(superHeroTags, function(i, value){
+            $("#"+value).click(function () {     
+             if ($("#"+value+"Data").is(':hidden')) {
+                 $(".superhero").css("background-color","#484848");
+                 $("#"+value).css("background-color","#666666");
+                 $(".superheroData").slideUp("slow");
+                 $("#"+value+"Data").slideDown("slow");
+             } else {
+                 $("#"+value+"Data").slideUp("slow");
+                 $("#"+value).css("background-color","#484848");          
+             }
+         });
+        });     
+        
     });
-    alert("s");
-   superHeroTags= ["fantastic","invisible","torch","thing"];        // cuando funcione lo de leer del json, suprimir esta linea
-    
-   
-   // fill the divs with the json content
-   
-   // function to show data of the superheroes 
-   $.each(superHeroTags, function(i, value){
-       $("#"+value).click(function () {     
-        if ($("#"+value+"Data").is(':hidden')) {
-            $(".superhero").css("background-color","#484848");
-            $("#"+value).css("background-color","#666666");
-            $(".superheroData").slideUp("slow");
-            $("#"+value+"Data").slideDown("slow");
-        } else {
-            $("#"+value+"Data").slideUp("slow");
-            $("#"+value).css("background-color","#484848");          
-        }
-    });
-   });     
 });
 
 
