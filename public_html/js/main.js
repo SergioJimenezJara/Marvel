d010f84a1e6bf51e8b73f91d3467101a68cd9280
unknown <chivo.sergio.jesus@gmail.com>
2018-12-19 17:48:10 +0100

    read json and show superheros data (doesnt work)

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -4,23 +4,22 @@ $(document).ready(function () {
   // read the json with the superheroes
     $.getJSON('../js/avengers.json', function (data) {              // no esta probada, posible errores
         // iterate and append the data
-       
         $.each(data, function(i, obj) {
             // create a list with the tags    
             superHeroTags.push(obj.tag);
-            
             // append the json's data
-            $.("."+obj.tag+"Name").append(obj.name+"");
-            $.("."+obj.tag+"History").append(obj.history+"");
+            $("."+obj.tag+"Name").append(obj.name+"");
+            $("."+obj.tag+"History").append(obj.history+"");
             
             // read multiple powers
             $.each(obj.powers, function(i, power){
-                $.("."+obj.tag+"Powers").append(power+" ");
-            });        
+                $("."+obj.tag+"Powers").append(power+" ");
+            });  
+                  
             
         });
     });
-    
+    alert("s");
    superHeroTags= ["fantastic","invisible","torch","thing"];        // cuando funcione lo de leer del json, suprimir esta linea
     
    
